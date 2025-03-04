9b97199f50376d9671941f916114f7d904773aed
Jvillegasd <johnnyvillegaslrs@gmail.com>
2025-03-03 23:47:02 -0500

    feat: edited setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='youtube-dl-cli',
+    name='media-bridge',
     version='0.1.0',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
@@ -10,15 +10,15 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'youtube-dl-cli=cli:main',
+            'media-bridge=cli:main',
         ],
     },
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='jvillegasd',
+    author_email='johnnyvillegaslrs@gmail.com',
     description='A command-line interface for downloading videos using youtube-dl.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/youtube-dl-cli',
+    url='https://github.com/jvillegasd/media-bridge',
     classifiers=[
         'Programming Language :: Python :: 3.12',
         'License :: OSI Approved :: MIT License',
