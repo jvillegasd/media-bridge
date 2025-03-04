80ce73cf6c2532b5e6fe55241c82b9c06b10fce7
Jvillegasd <johnnyvillegaslrs@gmail.com>
2025-03-04 00:35:36 -0500

    feat: added docstring

--- src/cli.py
+++ src/cli.py
@@ -2,6 +2,7 @@ import argparse
 from downloader import Downloader
 
 def main():
+    'python ./src/cli.py "https://www.youtube.com/watch?v=967rftQk7zs"'
     parser = argparse.ArgumentParser(description='Download videos using youtube-dl.')
     parser.add_argument('url', type=str, help='The URL of the video to download.')
     parser.add_argument('-o', '--output', type=str, help='The output file name (optional).')
